77eeecd3ec488e4e729b725c0e58da6b891e6049
Amogh Kulkarni <amogh.s.kulkarni@vanderbilt.edu>
2017-07-08 02:56:12 -0500

    Minor success with continuous time simulation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fireflies_des import FirefliesSimulation
+from fireflies_continuous import FirefliesSimulation
 
 ff_sim = FirefliesSimulation(100, neighbor_distance=100)
 ff_sim.start_simulation()
