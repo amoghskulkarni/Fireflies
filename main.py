548104edf2b3ced26ae5cef446bc64ef85f94905
Amogh Kulkarni <amogh.s.kulkarni@vanderbilt.edu>
2017-07-09 17:50:07 -0500

    Starting the work on moving fireflies

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fireflies_continuous import FirefliesSimulation
+from static_fireflies_continuous import FirefliesSimulation
 
 ff_sim = FirefliesSimulation(100, period=30, nudge=5, neighbor_distance=100)
 ff_sim.start_simulation()
