e181c57bb5037719e9a1b5d475db3214e159181c
Amogh Kulkarni <amogh.s.kulkarni@vanderbilt.edu>
2017-07-14 02:08:51 -0500

    Started working on Strogatzian models of fireflies with only positive nudges and non-linear clock update

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from static_fireflies_adversarial import FirefliesSimulation
+# from static_fireflies_adversarial import FirefliesSimulation
 # from moving_fireflies_adversarial import FirefliesSimulation
 # from static_fireflies import FirefliesSimulation
 # from moving_fireflies import FirefliesSimulation
+from static_fireflies_strogatzian import FirefliesSimulation
 
 
 ff_sim = FirefliesSimulation(period=60, nudge=10, neighbor_distance=150)
